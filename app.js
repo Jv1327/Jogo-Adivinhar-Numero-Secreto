f4bc5f1a095ae2eb28456eabed65370c07875758
Jv1327 <jvrodrigues132770@gmail.com>
2024-07-03 20:26:04 -0300

    Arrumando projeto

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 alert('Boas Vindas ao Jogo do Número Secreto');
-
-let maxRange = 5000;  
-let numeroMaximo = parseInt(Math.random() * maxRange + 1);  
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); 
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = parseInt(prompt(`Escolha um Número entre 1 e ${numeroMaximo}`));
+    chute = prompt(`Escolha um Número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou o Número Secreto ${numeroSecreto} em ${tentativas} tentativas!`);
         break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O Número Secreto é menor que ${chute}`);
-        } else {
+        }
+        else {
             alert(`O Número Secreto é maior que ${chute}`);
         }
         tentativas++;
     }
 }
-
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 alert(`Isso ai!! Você descobriu o número Secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
